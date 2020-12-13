fc275d86c47481fe192b2382fa3eb3bbb071e4e0
Alikhan <amadeus-8@yandex.ru>
2020-12-13 17:51:45 +0600

    Доделал логику регистрации Добавил стили Добавил валидацию и прочее

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require('express')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const config = require('../config')
+const config = require('config')
 const { check, validationResult } = require('express-validator')
 const User = require('../models/User')
 const router = Router()
